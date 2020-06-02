a8b568e963c12b1ebd97246c217bbd4d2ffe2275
Jaco Joubert <jaco@outdoorsy.com>
2020-06-02 09:31:41 -0400

    build(check in build): check in build

---
+++ dist/index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var vm = _interopDefault(require('vm'));
+var fs = _interopDefault(require('fs'));
+var path = _interopDefault(require('path'));
+
+function wrapModuleExports(code) {
+  return `
+    function getModuleExports() {
+      const module = {};
+
+      ${code}
+
+      return module.exports;
+    }
+  `;
+}
+
+function defaultExport(html, css) {
+  const style = css ? `<style>${css}</style>` : "";
+  return `${style}${html}`;
+}
+
+const defaultOptions = {
+  /** do not emit SSR bundle */
+  skipEmit: false,
+  configureExport: defaultExport,
+};
+
+/** */
+function ssr(options = {}) {
+  const pluginOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  if (!pluginOptions.fileName) {
+    throw new Error("options.fileName should be string or function");
+  }
+
+  return {
+    name: "svelte-ssr",
+    async generateBundle(config, bundle, isWrite) {
+      if (config.format !== "cjs") {
+        throw new Error("rollup-plugin-svelte-ssr can only be used with 'cjs'-format");
+      }
+
+      const destPath = path.relative("./", config.file);
+      const destDir = destPath.slice(0, destPath.lastIndexOf(path.sep));
+
+      Object.keys(bundle).forEach(async key => {
+        const entry = bundle[key];
+
+        const sandbox = {
+          ssr: {
+            html: "",
+            css: "",
+          },
+        };
+
+        try {
+          const props = JSON.stringify(pluginOptions.props, null, 2);
+          const generateSsrScript = `
+            ${wrapModuleExports(entry.code)}
+            const App = getModuleExports();
+            const { html, css } = App.render(${props});
+            ssr.html = html;
+            ssr.css = css.code;
+          `;
+          const script = new vm.Script(generateSsrScript);
+          script.runInNewContext(sandbox);
+        } catch (e) {
+          throw e;
+        }
+
+        const html =
+          typeof pluginOptions.preprocessHtml === "function"
+            ? pluginOptions.preprocessHtml(sandbox.ssr.html)
+            : sandbox.ssr.html;
+
+        const css =
+          typeof pluginOptions.preprocessCss === "function"
+            ? pluginOptions.preprocessCss(sandbox.ssr.css)
+            : sandbox.ssr.css;
+
+        const fileName =
+          typeof pluginOptions.fileName === "function"
+            ? pluginOptions.fileName(entry)
+            : pluginOptions.fileName;
+
+        const destination = path.resolve(destDir, fileName);
+        fs.mkdirSync(path.dirname(destination), { recursive: true });
+        fs.writeFileSync(destination, pluginOptions.configureExport(html, css));
+
+        if (pluginOptions.skipEmit) {
+          // You can prevent files from being emitted by deleting them from the bundle object.
+          delete bundle[key];
+        }
+      });
+    },
+  };
+}
+
+module.exports = ssr;
